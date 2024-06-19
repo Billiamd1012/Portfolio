e2765463813322b7a4f5a1dba830f6d859205837
Billiamd1012 <william.darker2003@gmail.com>
2024-06-19 14:04:20 +0930

    Added experience.json

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    with open('experience.json') as f:
+        experiences = json.load(f)
+    return render_template('index.html', experiences=experiences['experiences'])
+
+
+# Route to serve the resume PDF
+@app.route('/resume.pdf')
+def serve_pdf():
+    return send_from_directory('static/dist/pdf', 'resume.pdf')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
